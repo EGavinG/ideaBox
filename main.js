8bc3562a01b164f6cc3401c216434aa47b84f332
Brendan Turner <kisbyturner@gmail.com>
2023-09-13 10:58:15 -0600

    Add script tag and completes createIdea function

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 //Variables & Data
-var saveButton = document.querySelector('save-button');
+var form = document.querySelector("form");
+var saveButton = document.querySelector('.save-button');
 var newIdeaTitle = document.querySelector('#idea-title');
 var newIdeaBody = document.querySelector('#idea-body');
 
-var ideas = []
+var ideas = [];
 
 
 //Event Listeners
 
-saveButton.addEventListener('click', displayIdea)
+form.addEventListener("submit", displayIdea);
 
 //Functions
+function displayIdea(event) {
+    var tempIdea = {}
+    tempIdea = createIdea(newIdeaTitle.value, newIdeaBody.value);
+    event.preventDefault();
+}
 
 function createIdea(title, body) {
     var idea = {}
-    idea.title = title.value;
-    idea.body = body.value;
+    idea.title = title;
+    idea.body = body;
     idea.id = Date.now();
     ideas.push(idea);
-}
-
-function displayIdea(event) {
-    var tempIdea = {}
-    event.preventDefault();
-    tempIdea = createIdea(newIdeaTitle.value, newIdeaBody.value);
+    console.log(idea);
+    return idea;
 }
