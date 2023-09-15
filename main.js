cee971f9d9a20d87eeb25335ec5e4d189d78bf8e
Gavin Garcia <edwardgavingarcia@yahoo.com>
2023-09-15 17:35:40 -0500

    Fix innerHTML for cards/get hover states working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Variables & Data
 var form = document.querySelector("form");
-var showFavoriteButton = document.querySelector('.starred-idea-button')
+var showFavoriteButton = document.querySelector(".starred-idea-button");
 var saveButton = document.querySelector(".save-button");
 var newIdeaTitle = document.querySelector("#idea-title");
 var newIdeaBody = document.querySelector("#idea-body");
@@ -10,7 +10,7 @@ var ideas = [];
 
 //Event Listeners
 
-showFavoriteButton.addEventListener('click', showFavoriteCards)
+showFavoriteButton.addEventListener("click", showFavoriteCards);
 form.addEventListener("submit", displayIdea);
 cardField.addEventListener("click", clickEvent);
 
@@ -40,23 +40,17 @@ function createIdeaCards() {
 }
 
 function createIdeaCard(i) {
-    var card = `<article class='card-instance' id=${ideas[i].id}>
+  var card = `<article class='card-instance' id=${ideas[i].id}>
               <header class='card-header'>
                   <div class='button-field'>
                       <button class='fav-button' value=${ideas[i].favorite}>
-                          <svg class='fav-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.06 15.65">
+                          <svg class='fav-icon' xmlns="http://www.w3.org/2000/svg" value= "fav-icon">
                               <defs><style>.cls-1{fill:#fff;}</style></defs>
-                              <g id="background">
-                                  <polygon class="cls-1" points="8.02 2.48 9.64 5.77 13.27 6.29 10.64 8.85 11.26 12.47 8.02 10.76 4.77 12.47 5.39 8.85 2.76 6.29 6.39 5.77 8.02 2.48"/>
-                              </g>
                           </svg>
                       </button>
                       <button class='delete-button' data-id=${ideas[i].id}>
-                          <svg class='delete-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.18 17.72">
+                          <svg class='delete-icon' xmlns="http://www.w3.org/2000/svg" value= "delete-icon">
                               <defs><style>.cls-1{fill:#fff;}</style></defs>
-                              <g id="background">
-                                  <polygon class="cls-1" points="13.13 5.87 12.43 5.16 8.97 8.61 5.53 5.16 4.82 5.87 8.27 9.32 4.82 12.77 5.53 13.48 8.97 10.03 12.43 13.48 13.13 12.77 9.68 9.32 13.13 5.87"/>
-                              </g>
                           </svg>
                       </button>
                   </div>
@@ -66,17 +60,16 @@ function createIdeaCard(i) {
                   <p>${ideas[i].body}</p>
               </div>
           </article>`;
-    return card;
+  return card;
 }
 function createFavoriteCards() {
-    cardField.innerHTML = '';
-    for(i = 0; i<ideas.length; i++) {
-        if(ideas[i].favorite === 'true')
-            createIdeaCard(i);
-    }
+  cardField.innerHTML = "";
+  for (i = 0; i < ideas.length; i++) {
+    if (ideas[i].favorite === "true") createIdeaCard(i);
+  }
 }
 function clickEvent(e) {
-    console.log(e.target.value)
+  console.log(e.target.value);
   if (e.target.classList.contains("delete-button")) {
     deleteCard(e);
   } else if (e.target.classList.contains("fav-button")) {
